f75c382ab09376b8ddd02d12838fb796027c46d2
Bradley Katz <katz0210@p--cs-mini-534.wlu.ca>
2018-03-15 16:18:55 -0400

    Initial Commit

---
+++ katz0210_a6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  katz0210_a6
+//
+//  Created by Bradley Katz on 2018-03-15.
+//  Copyright © 2018 Bradley Katz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
